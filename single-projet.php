55d4e8d74068b26151d44e8687b11f2794f7d332
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-11 16:55:16 +0200

    correction single-projet

--- single-projet.php
+++ single-projet.php
@@ -25,45 +25,6 @@ $imgSep = get_field('image-separator');
     </div>
 </div>
 
-<header id="header" >
-    <img src="<?php echo $bg_url;?>" alt="<?php echo $bg_header['title'];?>"/>
-    <div class="container">
-        <?php if($surtitre): echo '<span class="subtitle">'.$surtitre.'</span>'; endif;?>
-        <?php if($titre): echo $titre; endif;?>
-        <?php if($price): echo '<span class="cta">ÀPD DE '.$price.'€ LA NUIT';endif;?>
-    </div>
-
-    <div class="booking">
-        <form class="research-bar" action="<?php echo get_bloginfo('url').'/booking';?>" method="POST">
-            <div class="research-bar__item">
-                <select class="research-bar__item--input gites" name="gites">
-                    <option value="la-chapelle">La Chapelle</option>
-                    <option value="la-passerelle">La Passerelle</option>
-                </select>
-            </div>
-            <div class="research-bar__item">
-                <input class="research-bar__item--input date" type="text" name="check-in" id="check-in" placeholder="Date d'arrivée" />
-            </div>
-            <div class="research-bar__item">
-                <input class="research-bar__item--input date" type="text" name="check-out" id="check-out" placeholder="Date de départ" />
-            </div>
-            <div class="research-bar__item">
-                <input class="research-bar__item--input people" type="text" name="people" id="people" placeholder="Personnes" />
-            </div>  
-            <div class="research-bar__item button">
-                <input type="submit" id="rechercher" value="Rechercher" />
-            </div>
-        </form>
-    </div>
-
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <script>
-    jQuery(document).ready(function($){
-        $("#check-in, #check-out").datepicker({ dateFormat: 'dd/mm/yy' });
-    });
-    </script>
-</header>
 
 <section id="simple-page">
     <div class="container">

8c9f78dde53dbce9115f2181b3c02466b6f06734
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-26 16:13:48 +0200

    Nouvelle section page apropos

--- a-propos.php
+++ a-propos.php
@@ -7,6 +7,32 @@ get_header();
 <?php get_template_part( 'templates-parts/section-introduction' );?>
 <?php get_template_part( 'templates-parts/section-galerie' );?>
 <?php get_template_part( 'templates-parts/section-blue' );?>
+
+<section id="section-service-2">
+    <div class="container columns">
+        <div class="col-g">
+            <?php 
+                $titre_section = get_field('titre-section-2');
+                $texte_section = get_field('texte-section-2');
+                $cta_section = get_field('cta-section-2');
+
+                if($titre_section): echo $titre_section; endif;
+                if($texte_section): echo $texte_section; endif;
+                if($cta_section): echo '<a href="'.$cta_section['url'].'" class="cta">'.$cta_section['title'].'</a>';endif;
+            ?>
+        </div>
+
+        <div class="col-d">
+            <?php 
+                $image_section = get_field('image-section-2');
+
+                if($image_section):?>
+                    <img src="<?php echo $image_section['url'];?>" alt="<?php echo $image_section['title'];?>" class="img-section-2"/>
+                <?php endif;?>
+        </div>
+    </div>
+</section>
+
 <?php get_template_part( 'templates-parts/section-prom-immo' );?>
 
 <?php get_footer();  

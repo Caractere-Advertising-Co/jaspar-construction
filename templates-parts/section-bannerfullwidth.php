97edd9c606dd19ad18f1b26c4727c6250a5f1221
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-29 15:21:46 +0200

    backgroundsize + background rerpeat

--- templates-parts/section-bannerfullwidth.php
+++ templates-parts/section-bannerfullwidth.php
@@ -5,7 +5,7 @@ $cta = get_field('cta-fullwidth','options');
 $bgBanner = get_field('background-fullwidth','options');
 ?>
 
-<section id="banner-fullwidth" <?php if($bgBanner): echo 'style="background-image:url('.$bgBanner['url'].');"';endif;?>>
+<section id="banner-fullwidth" <?php if($bgBanner): echo 'style="background-image:url('.$bgBanner['url'].');background-size:cover;background-repeat:no-repeat;"';endif;?>>
     <div class="container">
         <?php if($title) : echo $title;endif;?>
         <?php if($cta): echo '<a href="'.$cta['url'].'" class="cta">'.$cta['title'].'</a>'; endif;?>

61b0baefe41e17e4383b1bafc3e109fa564ad43d
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-29 15:19:11 +0200

    bannerfullwidth

--- templates-parts/section-bannerfullwidth.php
+++ templates-parts/section-bannerfullwidth.php
@@ -2,9 +2,10 @@
 $surtitre = get_field('surtitre-fullwidth','options');
 $title = get_field('title-fullwidth','options') ;
 $cta = get_field('cta-fullwidth','options');
+$bgBanner = get_field('background-fullwidth','options');
 ?>
 
-<section id="banner-fullwidth">
+<section id="banner-fullwidth" <?php if($bgBanner): echo 'style="background-image:url('.$bgBanner['url'].');"';endif;?>>
     <div class="container">
         <?php if($title) : echo $title;endif;?>
         <?php if($cta): echo '<a href="'.$cta['url'].'" class="cta">'.$cta['title'].'</a>'; endif;?>

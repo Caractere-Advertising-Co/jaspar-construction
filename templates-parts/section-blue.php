3ce67d6f0c6395f775824ea3b260571519be0d9f
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-29 13:38:41 +0200

    ajout image section bleu

--- templates-parts/section-blue.php
+++ templates-parts/section-blue.php
@@ -3,6 +3,7 @@
 $titre = get_field('titre-blue');
 $texte = get_field('texte-blue');
 $cta = get_field('cta-blue');
+$img_blue = get_field('img-blue');
 
 ?>
 
@@ -11,12 +12,20 @@ $cta = get_field('cta-blue');
         <div class="title">
             <?php if($titre): echo $titre; endif;?>
         </div>
-        
-        <div class="paragraph">
-            <?php if($texte) : echo $texte; endif;?>
-            <?php if($cta):?>
-                <a href="<?php echo $cta['url'];?>" class="cta"><?php echo $cta['title'];?></a>
-            <?php endif;?>
+    </div>
+
+    <div class="container columns">
+        <div class="col-g">
+            <img src="<?php echo $img_blue['url'];?>" alt="<?php $img_blue['title'];?>"/>
+        </div>
+
+        <div class="col-d">
+            <div class="paragraph">
+                <?php if($texte) : echo $texte; endif;?>
+                <?php if($cta):?>
+                    <a href="<?php echo $cta['url'];?>" class="cta"><?php echo $cta['title'];?></a>
+                <?php endif;?>
+            </div>
         </div>
     </div>
 </section>

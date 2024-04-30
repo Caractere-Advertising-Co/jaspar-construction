e43a50790bd04e7f821f0082982647988fbd92a3
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-30 11:31:38 +0200

    correction popup

--- templates-parts/section-popup.php
+++ templates-parts/section-popup.php
@@ -7,20 +7,18 @@
 
         if($popup):?>
             <div class="modal_popup" id="modal_popup_front">
-                <a href="<?php if($cta):echo $cta['url'];endif?>" class="centerPopup">
-                    <div class="content_popup">            
-                        <div class="col-g">
-                            <?php if($imagePu):?>
-                                <img src="<?php echo $imagePu['url'];?>" alt="<?php echo $imagePu['title'];?>"/>
-                            <?php endif;?>    
-                        </div>
-                        <div class="col-d">
-                            <div class="close_popup" id="close_popup">x</div>
-                            <?php if($contentPopUp): echo $contentPopUp; endif;?>
-                            <?php if($cta): echo '<a href="'.$cta['url'].'">'.$cta['title'].'</a>';endif;?>
-                        </div>
+                <div class="content_popup">            
+                    <div class="col-g">
+                        <?php if($imagePu):?>
+                            <img src="<?php echo $imagePu['url'];?>" alt="<?php echo $imagePu['title'];?>"/>
+                        <?php endif;?>    
                     </div>
-                </a>
+                    <div class="col-d">
+                        <div class="close_popup" id="close_popup">x</div>
+                        <?php if($contentPopUp): echo $contentPopUp; endif;?>
+                        <?php if($cta): echo '<a href="'.$cta['url'].'">'.$cta['title'].'</a>';endif;?>
+                    </div>
+                </div>
             </div>
         <?php endif;?>
 </section>

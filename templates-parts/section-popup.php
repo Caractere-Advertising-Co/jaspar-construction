621662a7204a087b58e5c539213b375695a64027
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-04-30 11:30:02 +0200

    correction popup

--- templates-parts/section-popup.php
+++ templates-parts/section-popup.php
@@ -7,23 +7,20 @@
 
         if($popup):?>
             <div class="modal_popup" id="modal_popup_front">
-
-
-                <div class="centerPopup">
-                    <div class="close_popup" id="close_popup">x</div>
-                    
-                    <a href="<?php if($cta):echo $cta['url'];endif?>" class="content_popup">
+                <a href="<?php if($cta):echo $cta['url'];endif?>" class="centerPopup">
+                    <div class="content_popup">            
                         <div class="col-g">
                             <?php if($imagePu):?>
                                 <img src="<?php echo $imagePu['url'];?>" alt="<?php echo $imagePu['title'];?>"/>
                             <?php endif;?>    
                         </div>
                         <div class="col-d">
+                            <div class="close_popup" id="close_popup">x</div>
                             <?php if($contentPopUp): echo $contentPopUp; endif;?>
                             <?php if($cta): echo '<a href="'.$cta['url'].'">'.$cta['title'].'</a>';endif;?>
                         </div>
-                    </a>
-                </div>
+                    </div>
+                </a>
             </div>
         <?php endif;?>
 </section>
